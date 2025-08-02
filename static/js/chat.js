42ad9cd8b939c4d168a3348b5e8c7a8e6c9a2b8f
MathiMomos <mathi.gamer.m@gmail.com>
2025-08-02 04:10:56 -0500

    minor tweaks to css/js

--- static/js/chat.js
+++ static/js/chat.js
@@ -71,8 +71,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     globalMessages.addEventListener('click', function(event) {
         // Busca el botón más cercano que coincida con el selector
-        const button = event.target.closest('.generate-image-button');
-        if (button) {
+        const imagebutton = event.target.closest('.generate-image-button');
+        const diagrambutton = event.target.closest('.generate-diagram-button');
+        const chartbutton = event.target.closest('.generate-chart-button');
+        if (imagebutton || diagrambutton || chartbutton) {
             generateImage();
         }
     });
@@ -326,10 +328,10 @@ document.addEventListener('DOMContentLoaded', function () {
             <button class="bot-action-button generate-image-button" title="Generar Imagen">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21zm0-2h14V5H5zm1-2h12l-3.75-5l-3 4L9 13zm-1 2V5z"/></svg>
             </button>
-            <button class="bot-action-button" title="Botón 2">
+            <button class="bot-action-button generate-diagram-button" title="Generar Diagrama">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5zM8.5 5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5zM0 11.5A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm4.5.5A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm4.5.5a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z"/></svg>
             </button>
-            <button class="bot-action-button" title="Botón 3">
+            <button class="bot-action-button generate-chart-button" title="Generar Estadistica">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 21H2V3h2v16h2v-9h4v9h2V6h4v13h2v-5h4z"/></svg>
             </button>
         `;
